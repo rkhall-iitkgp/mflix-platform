8567b96a007b381735467eb8f5d3b9e7e622307c
UtkarshBhatt6 <utkarshbhatt@kgpian.iitkgp.ac.in>
2024-03-17 01:37:01 +0530

    added colors and sizes

--- assets/themes/colors.tsx
+++ assets/themes/colors.tsx
@@ -1,6 +1,22 @@
 const themeOptions = {
-    LIGHT: 'light',
-    DARK: 'dark',
+    color: {
+        button: '#7011B6',
+        divider: '#FFFFFF',
+        largeBox: '#BFBFBF',
+        smallBox: '#9341D0',
+        textColorNormal: '#9441D0',
+        insideTextColor: '#828282',
+        normalTextColor: '#FFFFFF',
+    },
+    fontSize: {
+        xs: '0.75rem',
+        s: '1rem',
+        l: '2rem',
+        xl: '3rem',
+        xxl: '4rem',
+
+    }
+
 }
 
 export default themeOptions;

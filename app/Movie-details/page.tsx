b3f9464a5f644e9c204a1ee492a9f1527cb5b417
Harsh-Sinha <harsh2004sinha@gmail.com>
2024-03-19 02:09:26 +0530

    SimilarMovies Half Done

--- app/Movie-details/page.tsx
+++ app/Movie-details/page.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import { Group, Button, Image, Stack, Text, Paper } from '@mantine/core';
 import { GrLocation } from 'react-icons/gr';
 import { FaRegHourglass } from 'react-icons/fa6';
@@ -9,92 +11,112 @@ import { FaPlus } from 'react-icons/fa';
 import themeOptions from '@/utils/colors';
 import Sample from '@/assets/sample.png';
 import { IoIosArrowForward } from 'react-icons/io';
-import MovieCard from '../../components/MovieDetails/MovieCard';
 import { createStyles } from '@mantine/styles';
+import SimilarMovies from '@/components/MovieDetails/SimilarMovies';
 
-export default function MovieBanner() {
+export default function MovieDetails() {
     const genres = ['Mystery', 'Action', 'Thriller'];
     const director = ['James Cameron'];
     const writer = ['Rick Jaffa'];
     const cast = ['Someone'];
 
-    // const styles = createStyles(()=>({
+    const styles = createStyles(() => ({
+        one: {
+            width: '100%',
+            height: '100vh',
+            border: '1px solid #fff',
+            marginTop: '150px',
+            background: '#000',
+        },
+        two: {
+            marginTop: "30px", 
+            paddingLeft: "40px",
+        },
+        three:{
+            width:'7rem',
+            paddingTop:'5px',
+            paddingBottom:'5px',
+        },
+        four:{
+            width:'500px',
+        },
+        five:{
+            marginTop: "50px", 
+            border: "1px solid", 
+            borderRadius: "10px", 
+            padding: "10px", 
+            alignItems: "center"
+        },
+        six:{
+            maxWidth:"max-content",
+        },
+        seven:{
+            marginLeft: "70px", marginTop: "200px", color:'#000',
+        },
+        eight:{
+            border: "1px solid #000", borderRadius: "10px", paddingInline: "15px", background: "#000", opacity: "40%",
+            color:'#fff'
+        }
 
 
-    // }))
+    }))
+
+    const { classes } = styles();
 
     return (
         <Stack
-        //   style={{
-        //     maxWidth: '600px',
-        //   }}
-        //   styles={{
-        //     root: {
-        //         borderImageSource: 'linear-gradient(166.93deg, #B586D8 3.24%, rgba(143, 72, 196, 0) 96.43%), linear-gradient(317.92deg, rgba(255, 255, 255, 0.6) 1.48%, rgba(112, 17, 182, 0.09) 67.95%)',
-        //         borderImageSlice: '1',
-        //         borderImageWidth: '1',
-        //         borderImageOutset: '0',
-        //         borderRadius: '20px',
-        //         background: 'linear-gradient(0deg, rgba(112, 17, 182, 0.15), rgba(112, 17, 182, 0.15)), linear-gradient(321.23deg, rgba(112, 17, 182, 0.2) 5.98%, rgba(0, 0, 0, 0) 66.28%)',
-        //     },
-        //   }}
-          justify="space-between"
-          pt="md"
-          pb="xs"
-          pl={themeOptions.fontSize.l}
-          pr={themeOptions.fontSize.l}
-          gap={0}
+            justify="space-between"
+            pt="md"
+            pb="xs"
+            pl={themeOptions.fontSize.l}
+            pr={themeOptions.fontSize.l}
+            gap={0}
         >
-            <Group style={{width:"100%", height:"100vh", border:"1px solid #fff", marginTop:"150px", background:"#fff"}}>
+            <Group className={classes.one}>
 
             </Group>
             <Group align="center">
                 <div>
                     <Image
-                      src={Sample}
-                      component={NextImage}
-                      style={{height:"430px", width:"291px", marginTop:"60px", marginLeft:"100px"}}
-                      alt="sample"
-                      height={158}
+                        src={Sample}
+                        component={NextImage}
+                        // className={classes.two}
+                        style={{ height: "430px", width: "291px", marginTop: "60px", marginLeft: "100px" }}
+                        alt="sample"
                     />
                 </div>
-                <Stack ml="xxl" gap="xs" mr="lg" style={{marginTop:"30px" , paddingLeft:"40px"}}>
+                <Stack ml="xxl" gap="xs" mr="lg" className={classes.two}>
                     <Text fz={themeOptions.fontSize.xxl}>Movie Name</Text>
                     <Group gap={themeOptions.fontSize.xs}>
                         {genres.map((e, i) =>
-                        <Paper
-                          key={i}
-                          bg={themeOptions.color.smallBox}
-                          fz={themeOptions.fontSize.s}
-                        //   w={116}
-                          pt={5}
-                          pb={5}
-                          radius={13}
-                          style={{
-                            width:'5rem'
-                          }}
-                        >
-                            <Text ta="center">{e}</Text>
-                        </Paper>)}
+                            <Paper
+                                key={i}
+                                bg={themeOptions.color.smallBox}
+                                fz={themeOptions.fontSize.s}
+                                radius={13}
+                                className={classes.three}
+                            >
+                                <Text ta="center">{e}</Text>
+                            </Paper>)}
                     </Group>
-                    <Group mt={7} justify="space-between" style={{width:"500px"}}>
+                    <Group mt={7} justify='space-between' className={classes.four}>
                         <Group gap={6} justify="space-around">
                             <Image
-                              src={ImdbImg}
-                              component={NextImage}
-                              alt="imdb"
-                              height={20}
-                              unoptimized
+                                src={ImdbImg}
+                                component={NextImage}
+                                alt="imdb"
+                                height={20}
+                                unoptimized
                             />
                             <Text fz={themeOptions.fontSize.xs}>8.6/10</Text>
                         </Group>
                         <Group gap={6} justify="space-around">
                             <Image
-                              src={TomatoImg}
-                              component={NextImage}
-                              alt="imdb"
-                              height={20}
-                              unoptimized
+                                src={TomatoImg}
+                                component={NextImage}
+                                alt="imdb"
+                                height={20}
+                                unoptimized
                             />
                             <Text fz={themeOptions.fontSize.xs}>97 %</Text>
                         </Group>
@@ -106,79 +128,83 @@ export default function MovieBanner() {
                             <PiCalendar />
                             <Text fz={themeOptions.fontSize.xs}>2024</Text>
                         </Group>
-                        <Group gap={6} justify="space-around" style={{ maxWidth: 'max-content' }}>
+                        <Group gap={6} justify="space-around" className={classes.six}>
                             <GrLocation />
                             <Text fz={themeOptions.fontSize.xs}>Country</Text>
                         </Group>
                         <Group>
-                            <Text fz={themeOptions.fontSize.s} style={{fontWeight:"600"}}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum eaque provident, corrupti voluptatem, ullam nihil quaerat tenetur itaque libero dignissimos error. Fugit sint voluptates, aut placeat vitae animi eos amet ipsa distinctio neque quidem, nesciunt laborum quasi culpa, qui sapiente hic provident. Ratione perspiciatis ullam corporis voluptatum voluptatibus aperiam reprehenderit.</Text>
+                            <Text fz={themeOptions.fontSize.s} style={{ fontWeight: "600" }}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum eaque provident, corrupti voluptatem, ullam nihil quaerat tenetur itaque libero dignissimos error. Fugit sint voluptates, aut placeat vitae animi eos amet ipsa distinctio neque quidem, nesciunt laborum quasi culpa, qui sapiente hic provident. Ratione perspiciatis ullam corporis voluptatum voluptatibus aperiam reprehenderit.</Text>
                         </Group>
-                        <Group c={themeOptions.color.button} style={{marginTop:"50px", border:"1px solid", borderRadius:"10px",padding:"10px", alignItems:"center"}}>
-                            <Text style={{color:"#fff"}}>Add To WatchList</Text>
-                            <FaPlus/>
+                        <Group c={themeOptions.color.button} className={classes.five}>
+                            <Text style={{ color: "#000", fontWeight:'500' }}>Add To WatchList</Text>
+                            <FaPlus />
                         </Group>
                     </Group>
                 </Stack>
-                <Stack  style={{marginLeft:"70px", marginTop:"200px"}} color='#fff'>
-                    <Group style={{border:"1px solid #fff", borderRadius:"10px", paddingInline:"15px", background:"#fff", opacity:"40%"}}>
+                <Stack className={classes.seven}>
+                    <Group className={classes.eight}>
                         <Group display={'block'}>
-                            <Text fz={themeOptions.fontSize.md} style={{marginBottom:"-10px"}}> Director </Text>
+                            <Text fz={themeOptions.fontSize.md} style={{ marginBottom: "-10px" }}> Director </Text>
                             {director.map((e, i) =>
-                            <Paper
-                            style={{backgroundColor:'#fff'}}
-                            key={i}
-                            fz={themeOptions.fontSize.md}
-                            w={116}
-                            pt={5}
-                            pb={5}
-                            radius={13}
-                            
-                            >
-                                <Text ta="left">{e}</Text>
-                            </Paper>)}
+                                <Paper
+                                    style={{ backgroundColor: '#fff' }}
+                                    key={i}
+                                    fz={themeOptions.fontSize.md}
+                                    w={116}
+                                    pt={5}
+                                    pb={5}
+                                    radius={13}
+
+                                >
+                                    <Text ta="left">{e}</Text>
+                                </Paper>)}
                         </Group>
-                        <IoIosArrowForward fontSize={'70px'}/>
+                        <IoIosArrowForward fontSize={'70px'} />
                     </Group>
-                    <Group style={{border:"1px solid #fff", borderRadius:"10px", paddingInline:"15px", background:"#fff", opacity:"40%"}}>
+                    <Group className={classes.eight}>
                         <Group display={'block'} >
-                            <Text fz={themeOptions.fontSize.md} style={{marginBottom:"-10px"}}> Writers </Text>
+                            <Text fz={themeOptions.fontSize.md} style={{ marginBottom: "-10px" }}> Writers </Text>
                             {writer.map((e, i) =>
-                            <Paper
-                            style={{backgroundColor:'#fff'}}
-                            key={i}
-                            fz={themeOptions.fontSize.md}
-                            w={116}
-                            pt={5}
-                            pb={5}
-                            radius={13}
-                            >
-                                <Text ta="left">{e}</Text>
-                            </Paper>)}
+                                <Paper
+                                    style={{ backgroundColor: '#fff' }}
+                                    key={i}
+                                    fz={themeOptions.fontSize.md}
+                                    w={116}
+                                    pt={5}
+                                    pb={5}
+                                    radius={13}
+                                >
+                                    <Text ta="left">{e}</Text>
+                                </Paper>)}
                         </Group>
-                        <IoIosArrowForward fontSize={'70px'}/>
+                        <IoIosArrowForward fontSize={'70px'} />
                     </Group>
-                    <Group style={{border:"1px solid #fff", borderRadius:"10px", paddingInline:"15px", background:"#fff", opacity:"40%"}}>
+                    <Group className={classes.eight}>
                         <Group display={'block'}>
-                            <Text fz={themeOptions.fontSize.md} style={{marginBottom:"-10px"}}> Cast </Text>
+                            <Text fz={themeOptions.fontSize.md} style={{ marginBottom: "-10px" }}> Cast </Text>
                             {cast.map((e, i) =>
-                            <Paper
-                            style={{backgroundColor:'#fff'}}
-                            key={i}
-                            fz={themeOptions.fontSize.md}
-                            w={116}
-                            pt={5}
-                            pb={5}
-                            radius={13}
-                            
-                            >
-                                <Text ta="left">{e}</Text>
-                            </Paper>)}
+                                <Paper
+                                    style={{ backgroundColor: '#fff' }}
+                                    key={i}
+                                    fz={themeOptions.fontSize.md}
+                                    w={116}
+                                    pt={5}
+                                    pb={5}
+                                    radius={13}
+
+                                >
+                                    <Text ta="left">{e}</Text>
+                                </Paper>)}
                         </Group>
-                        <IoIosArrowForward fontSize={'70px'}/>
+                        <IoIosArrowForward fontSize={'70px'} />
                     </Group>
                 </Stack>
             </Group>
+            <Stack>
+                <SimilarMovies />
+            </Stack>
         </Stack>
-        
+
+
     );
 }
---
+++ components/MovieDetails/SimilarMovies.tsx
@@ -0,0 +1,190 @@
+'use client'
+
+import Image from 'next/image';
+import { useRef, useState, CSSProperties, useEffect } from 'react';
+import MovieCard from './MovieCard';
+import rArrow from '@/assets/icons/rarrow.svg';
+import Trend from '@/assets/icons/trends.svg';
+import { createStyles } from '@mantine/styles';
+// export default function SimilarMovies() {
+//     const scrollRef = useRef(null);
+//     const handleScrollRight = () => {
+//         console.log("scrolling")
+//         scrollRef.current.scrollBy({
+//             top: 0,
+//             left: 300, // Adjust this value based on the width of each item
+//             behavior: 'smooth',
+//         });
+//     };
+//     const handleScrollLeft = () => {
+//         console.log("scrolling")
+//         scrollRef.current.scrollBy({
+//             top: 0,
+//             left: -300, // Adjust this value based on the width of each item
+//             behavior: 'smooth',
+//         });
+//     };
+//     const [vis,setVis]=useState(false);
+//     return (
+//         <section className='pl-20  md:py-20 relative w-[calc(100vw-20px)]'>
+//             <h1 className='text-4xl mb-10 inline'>SimilarMovies</h1>
+//             <Image src={Trend} alt='icon' className='inline ml-2 pb-2' width={35} height={30} />
+//             <div ref={scrollRef} className='w-full overflow-hidden'>
+//                 <div className='flex w-max relative'>
+//                     <MovieCard />
+//                     <MovieCard />
+//                     <MovieCard />
+//                     <MovieCard />
+//                     <MovieCard />
+//                     <MovieCard />
+//                     <MovieCard />
+//                     <MovieCard />
+//                 </div>
+
+//             {vis && <Image src={rArrow} alt='slider' width={30} height={40} className='bg-[#1a202c] shadow-[-12px_20px_222px_200px_#1a202c] absolute left-40 top-1/2 transform -translate-y-1/2 cursor-pointer rotate-180' onClick={handleScrollLeft} />}
+//                     <Image src={rArrow} alt='slider' width={30} height={40} className='bg-[#1a202c] shadow-[-12px_20px_222px_200px_#1a202c] absolute right-10 top-1/2 transform -translate-y-1/2 cursor-pointer' onClick={handleScrollRight} />
+
+//             </div>
+//         </section>
+
+//     )
+// }
+
+
+export default function SimilarMovies() {
+    const scrollRef = useRef(null);
+    const [showLeftArrow, setShowFirstArrow] = useState(false);
+    const [vis, setVis] = useState(false);
+    useEffect(() => {
+        const handleScroll = () => {
+            // Check if the container is scrolled horizontally
+            if (scrollRef.current.scrollLeft > 100) {
+                setShowFirstArrow(true);
+            } else {
+                setShowFirstArrow(false);
+            }
+        };
+
+        // Add scroll event listener to the container
+        scrollRef.current.addEventListener('scroll', handleScroll);
+
+        // Remove event listener on component unmount
+        return () => {
+            scrollRef.current.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
+    const handleScrollRight = () => {
+        console.log("scrolling");
+        setVis(true);
+        scrollRef.current.scrollBy({
+            top: 0,
+            left: 300, // Adjust this value based on the width of each item
+            behavior: 'smooth',
+        });
+    };
+
+    const handleScrollLeft = () => {
+        console.log("scrolling")
+        scrollRef.current.scrollBy({
+            top: 0,
+            left: -300, // Adjust this value based on the width of each item
+            behavior: 'smooth',
+        });
+    };
+
+    const {classes, cx} = useStyles()
+
+    return (
+        <section className={classes.sectionStyles}>
+            <h1 className={classes.titleStyles}>SimilarMovies</h1>
+            <Image src={Trend} alt='icon' className={classes.iconStyles} />
+            <div ref={scrollRef} className={classes.containerStyles}>
+                <div className={classes.random}>
+                    <MovieCard />
+                    <MovieCard />
+                    <MovieCard />
+                    <MovieCard />
+                    <MovieCard />
+                    <MovieCard />
+                    <MovieCard />
+                    <MovieCard />
+                </div>
+                {showLeftArrow && (
+                    <Image
+                        src={rArrow}
+                        alt='slider'
+                        className={cx(classes.arrowStyles, classes.leftArrow, {opacity: showLeftArrow ? 1 : 0})}
+                        width={30}
+                        height={40}
+                        onClick={handleScrollLeft}
+                    />
+                )}
+                <Image
+                    src={rArrow}
+                    alt='slider'
+                    className={cx(classes.arrowStyles, classes.rightArrow)}
+                    width={30}
+                    height={40}
+                    onClick={handleScrollRight}
+                />
+            </div>
+        </section>
+    );
+}
+
+const useStyles = createStyles(() => ({
+    sectionStyles: {
+        paddingLeft: '80px',
+        paddingTop: '80px', // You may adjust the padding as needed
+        paddingBottom: '80px',
+        position: 'relative',
+        width: 'calc(100vw - 20px)',
+    },
+
+    titleStyles: {
+        fontSize: '2.25rem',
+        lineHeight: "2.5rem",
+        display: 'inline',
+        marginRight: "2.5rem",
+        marginTop:"2rem",
+        // fontWeight:"400",
+    },
+
+    iconStyles: {
+        marginLeft: '2rem',
+        paddingBottom: '2rem',
+        display: 'inline',
+    },
+
+    containerStyles: {
+        width: '100%',
+        overflow: 'hidden',
+        marginTop:'30px',
+    },
+
+    arrowStyles: {
+        backgroundColor: '#1a202c',
+        boxShadow: '-12px 20px 222px 200px #1a202c',
+        position: 'absolute',
+        top: '50%',
+        transform: 'translateY(-50%)',
+        cursor: 'pointer',
+    },
+
+    random: {
+        display: 'flex',
+        position: 'relative',
+        width: 'max-content',
+    },
+
+    leftArrow: {
+        left: '40px',
+        transform: 'translateY(-50%) rotate(180deg)',
+        transition: 'opacity 5s'
+    },
+
+    rightArrow: {
+        right: '10px'
+    }
+}))

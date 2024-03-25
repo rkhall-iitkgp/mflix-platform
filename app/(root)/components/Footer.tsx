553c768223a13390b8950d47df46a6a91cd14213
Ritik Raj <ritikraj1369@gmail.com>
2024-03-25 23:51:35 +0530

    Add @mantine/dates package and update Footer styles

--- app/(root)/components/Footer.tsx
+++ app/(root)/components/Footer.tsx
@@ -40,6 +40,64 @@ const styles = createStyles(() => ({
       color: 'gray',
     },
   },
+  // ulStyle: {
+  //   listStyleType: 'none',
+  //   marginTop: '0.5rem',
+  //   paddingLeft: '0',
+  //   textDecoration: 'none',
+  // },
+
+  // contactStylesRoot: {
+  //   width: '100%',
+  //   maxWidth: 300,
+  //   paddingLeft: 20,
+  //   marginTop: 32,
+  // },
+  // contactStylesTitle: {
+  //   color: 'gray',
+  //   fontWeight: 500,
+  //   marginBottom: 8,
+  //   fontSize: 18,
+  // },
+  // contactStylesLink: {
+  //   color: 'rgb(75 85 99)',
+  //   display: 'block',
+  //   marginBottom: '0.5rem',
+  //   textDecoration: 'none',
+  //   '&:hover': {
+  //     color: 'gray',
+  //   },
+  // },
+
+  // findUsStylesRoot: {
+  //   width: '100%',
+  //   maxWidth: 300,
+  //   paddingLeft: 20,
+  //   marginTop: 32,
+  // },
+  // findUsStylesTitle: {
+  //   color: 'gray',
+  //   fontWeight: 500,
+  //   marginBottom: 8,
+  //   fontSize: 18,
+  // },
+  // findUsStylesText: {
+  //   color: 'rgb(75 85 99)',
+  //   fontSize: '0.875rem',
+  //   lineHeight: '1.25rem',
+  //   marginBottom: 12,
+  //   marginTop: 0,
+  //   textDecoration: 'none',
+  // },
+  // findUsStylesLink: {
+  //   display: 'inline-flex',
+  //   alignItems: 'center',
+  //   marginTop: '0.5rem',
+  //   color: 'rgb(75 85 99)',
+  //   '&:hover': {
+  //     color: 'gray',
+  //   },
+  // },
   ulStyle: {
     listStyleType: 'none',
     marginTop: '0.5rem',
@@ -97,97 +155,37 @@ const styles = createStyles(() => ({
     '&:hover': {
       color: 'gray',
     },
-    ulStyle: {
-        listStyleType: 'none',
-        marginTop: '0.5rem',
-        paddingLeft: '0',
-        textDecoration: 'none',
-    },
+  },
+  pStyle: {
+    color: 'rgb(156, 163, 175)',
+    padding: '2.5rem',
+    fontSize: '0.875rem',
+    lineHeight: '1.25rem',
+  },
+  footStyle: {
+    color: 'white',
+    padding: '2rem',
+    paddingTop: '4rem',
+    paddingBottom: '2rem',
+    background: '#0B0212',
+  },
 
-    contactStylesRoot: {
-        width: '100%',
-        maxWidth: 300,
-        paddingLeft: 20,
-        marginTop: 32,
-    },
-    contactStylesTitle: {
-        color: 'gray',
-        fontWeight: 500,
-        marginBottom: 8,
-        fontSize: 18,
-    },
-    contactStylesLink: {
-        color: 'rgb(75 85 99)',
-        display: 'block',
-        marginBottom: '0.5rem',
-        textDecoration: 'none',
-        '&:hover': {
-            color: 'gray',
-        }
-
-    },
-
-
-        findUsStylesRoot: {
-            width: '100%',
-            maxWidth: 300,
-            paddingLeft: 20,
-            marginTop: 32,
-        },
-        findUsStylesTitle: {
-            color: 'gray',
-            fontWeight: 500,
-            marginBottom: 8,
-            fontSize: 18,
-        },
-        findUsStylesText: {
-            color: 'rgb(75 85 99)',
-            fontSize: '0.875rem',
-            lineHeight: '1.25rem',
-            marginBottom: 12,
-            marginTop: 0,
-            textDecoration: 'none'
-        },
-        findUsStylesLink: {
-            display: 'inline-flex',
-            alignItems: 'center',
-            marginTop: '0.5rem',
-            color: 'rgb(75 85 99)',
-            '&:hover': {
-                color: 'gray',
-            }
-        
-    },
-    pStyle: {
-        color: 'rgb(156, 163, 175)',
-        padding: '2.5rem',
-        fontSize: '0.875rem',
-        lineHeight: '1.25rem',
-    },
-    footStyle: {
-        color: 'white',
-        padding: '2rem',
-        paddingTop: '4rem',
-        paddingBottom: '2rem',
-        background:'#0B0212'
-    },
-
-    divStyle: {
-        width: '100%',
-        marginLeft: 'auto',
-        marginRight: 'auto',
-        paddingLeft: '1rem',
-        paddingRight: '1rem',
-        display: 'flex',
-        flexDirection: 'row',
-        justifyContent: 'space-between'
-    },
-    logoDivStyle: {
-        width: '100%',
-        paddingLeft: '2rem',
-        paddingRight: '2rem',
-        marginTop: '2rem',
-    }
+  divStyle: {
+    width: '100%',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    paddingLeft: '1rem',
+    paddingRight: '1rem',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  logoDivStyle: {
+    width: '100%',
+    paddingLeft: '2rem',
+    paddingRight: '2rem',
+    marginTop: '2rem',
+  },
 }));
 
 function Footer() {
--- app/(root)/components/Navbar.tsx
+++ app/(root)/components/Navbar.tsx
@@ -1,127 +1,126 @@
-'use client'
+'use client';
 
-import Link from 'next/link'
-import React from 'react'
-import { createStyles } from "@mantine/styles";
+import Link from 'next/link';
+import React from 'react';
+import { createStyles } from '@mantine/styles';
 import searchMsApiUrls from '../../api/searchMsApi';
+import themeOptions from '@/utils/colors';
 
 export default function Navbar() {
+  const handleLogOut = async () => {
+    const values = {
+      flag: 1,
+    };
+    const base_url = searchMsApiUrls();
+    const token = sessionStorage.getItem('accessToken');
+    await fetch(`${base_url}/auth/logout`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify({
+        ...values,
+      }),
+    }).then(async (res) => {
+      let jsonData = await res.json();
+      if (!res.ok) {
+        console.log(jsonData);
+      }
+      //   setLoading(false);
+      else {
+        sessionStorage.removeItem('accessToken');
+        console.log('Logout successful');
+        // console.log(jsonData);
+        // sessionStorage.setItem('accessToken', jsonData.user.accessToken);
+        // sessionStorage.setItem('token', jsonData.user.token);
+      }
+    });
+  };
 
-    const handleLogOut = async () => {
-        const values = {
-            "flag": 1,
-        }
-        const base_url = searchMsApiUrls()
-        const token = sessionStorage.getItem('accessToken');
-        await fetch(`${base_url}/auth/logout`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                'Authorization': `Bearer ${token}`
-            }, body: JSON.stringify({
-                ...values,
-            }),
-        }).then(async (res) => {
-            let jsonData = await res.json();
-            if (!res.ok) {
-                console.log(jsonData);
-            }
-            //   setLoading(false);
-            else {
-                sessionStorage.removeItem('accessToken');
-                console.log("Logout successful");
-                // console.log(jsonData);
-                // sessionStorage.setItem('accessToken', jsonData.user.accessToken);
-                // sessionStorage.setItem('token', jsonData.user.token);
-            }
-        });
-    }
+  const { classes } = useStyles();
+  return (
+    <nav className={classes.container}>
+      {/* Logo */}
+      <div className={classes.logoDiv}>
+        <img src="/logo.svg" alt="Logo" className={classes.logo} />
+      </div>
 
-
-    const { classes } = useStyles()
-    return (
-        <nav className={classes.container}>
-            {/* Logo */}
-            <div className={classes.logoDiv}>
-                <img src="/logo.svg" alt="Logo" className={classes.logo} />
-            </div>
-
-            {/* Links CSSPropertyRule*/}
-            <ul className={classes.links}>
-                <li>
-                    <Link href="#" className={classes.link}>
-                        Home
-                    </Link>
-                </li>
-                <li>
-                    <Link href="#" className={classes.link}>
-                        Login
-                    </Link>
-                </li>
-                <li className={classes.premium} >
-                    <Link href="#" className={classes.link2}>
-                        Premium
-                    </Link>
-                </li>
-            </ul>
-        </nav>
-    )
+      {/* Links CSSPropertyRule*/}
+      <ul className={classes.links}>
+        <li>
+          <Link href="#" className={classes.link}>
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link href="#" className={classes.link}>
+            Login
+          </Link>
+        </li>
+        <li className={classes.premium}>
+          <Link href="#" className={classes.link2}>
+            Premium
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
 const useStyles = createStyles(() => ({
-    container: {
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'space-between',
-        padding: '1rem',
-        paddingLeft: '2rem',
-        paddingRight: '2rem',
-        paddingTop: '0rem',
-        width: '100vw',
-        color: "white"
+  container: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    padding: '1rem',
+    paddingLeft: '2rem',
+    paddingRight: '2rem',
+    paddingTop: '0rem',
+    width: '100vw',
+    color: 'white',
+  },
+  logo: {
+    height: '2rem',
+    marginLeft: '1rem',
+  },
+  logoDiv: {
+    display: 'flex',
+    alignItems: 'center',
+    padding: '1rem',
+  },
+  links: {
+    display: 'flex',
+    padding: '1rem',
+    marginLeft: '1rem',
+    marginRight: '1rem',
+    listStyle: 'none',
+  },
+  link: {
+    padding: '1rem',
+    marginLeft: '1.5rem',
+    marginRight: '1.5rem',
+    textDecoration: 'none',
+    fontSize: '1.25rem',
+    color: 'white',
+    '&:hover': {
+      color: 'rgb(156, 163, 175)',
     },
-    logo: {
-        height: '2rem',
-        marginLeft: '1rem'
+    alignItems: 'center',
+  },
+  link2: {
+    padding: '0.5rem',
+    marginLeft: '1.5rem',
+    marginRight: '1.5rem',
+    textDecoration: 'none',
+    fontSize: '1.25rem',
+    '&:hover': {
+      color: 'rgb(156, 163, 175)',
     },
-    logoDiv: {
-        display: 'flex',
-        alignItems: 'center',
-        padding: '1rem'
-    },
-    links: {
-        display: 'flex',
-        padding: '1rem',
-        marginLeft: '1rem',
-        marginRight: '1rem',
-        listStyle: 'none',
-    },
-    link: {
-        padding: '1rem',
-        marginLeft: '1.5rem',
-        marginRight: '1.5rem',
-        textDecoration: 'none',
-        fontSize: '1.25rem',
-        color: 'white',
-        '&:hover': {
-            color: 'rgb(156, 163, 175)'
-        },
-        alignItems: 'center',
-    },
-    link2: {
-        padding: '0.5rem',
-        marginLeft: '1.5rem',
-        marginRight: '1.5rem',
-        textDecoration: 'none',
-        fontSize: '1.25rem',
-        '&:hover': {
-            color: 'rgb(156, 163, 175)'
-        },
-        alignItems: 'center',
-        border: '2px solid white',
-        borderRadius: '8px',
-        borderColor: themeOptions.color.smallBox,
-        color: themeOptions.color.smallBox,
-
-    }
-}))
+    alignItems: 'center',
+    border: '2px solid white',
+    borderRadius: '8px',
+    borderColor: themeOptions.color.smallBox,
+    color: themeOptions.color.smallBox,
+  },
+}));

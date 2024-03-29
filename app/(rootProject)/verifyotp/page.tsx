8517450aaddf3a89f980fc8e14a4faf6960e77bc
Yash Kumar <yash23kr@kgpian.iitkgp.ac.in>
2024-03-30 00:19:57 +0530

    Pricing cards and verifyotp page

--- app/(rootProject)/verifyotp/page.tsx
+++ app/(rootProject)/verifyotp/page.tsx
@@ -23,9 +23,6 @@ import searchMsApiUrls from '../api/searchMsApi';
 import useLoginStore from "@/Stores/LoginStore";
 import { useRouter } from "next/navigation";
 
-
-
-
 export function Otp({ initialValues }: any) {
     const router = useRouter()
     const [type, toggle] = useToggle(['login', 'register'])
@@ -68,14 +65,11 @@ export function Otp({ initialValues }: any) {
                 useLoginStore.getState().updateUser(jsonData.account);
                 const state = useLoginStore.getState();
                 console.log(state);
-                // router.push('/userprogfile')
+
             } else {
                 router.push('/login')
             }
-            // console.log(jsonData.account);
-            // console.log(jsonData);
-            // sessionStorage.setItem('sessionToken', jsonData.user.sessionToken);
-            // sessionStorage.setItem('token', jsonData.user.token);
+
         }
 
 
@@ -83,9 +77,32 @@ export function Otp({ initialValues }: any) {
     const handleOtpChange = (value: any) => {
         const newOtp = value;
         setOtpValue(newOtp);
-        // Update the initialValues object's otp field
         initialValues.otp = newOtp;
     };
+
+    const handleResendOtp = async () => {
+        setResendTime(60);
+        const base_url = searchMsApiUrls();
+        let res = await fetch(`${base_url}/auth/resendOTP`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                ...initialValues,
+            }),
+        })
+        let jsonData = await res.json();
+        if (!res.ok) {
+            console.log(jsonData.message);
+            // router.push('/verifyotp')
+        }
+        //   setLoading(false);
+        else {
+            console.log(jsonData.message);
+            console.log(jsonData)
+        }
+    }
 
     const form = useForm({
         initialValues: {
@@ -101,8 +118,7 @@ export function Otp({ initialValues }: any) {
 
         validate: {
             email: (val) => (/^\S+@\S+$/.test(val) ? null : 'Invalid email'),
-            password: (val) => (val.length <= 6 ? 'Password should include at least 6 characters' : null),
-
+            password: (val) => (val.length < 6 ? 'Password should include at least 6 characters' : null),
         },
     });
 
@@ -119,14 +135,19 @@ export function Otp({ initialValues }: any) {
             align="center"
             direction="column"
             wrap="wrap" >
-            {/* <BackgroundImage src='Group 18.png'> */}
             <Text size="2.5rem" c={'white'} p={'1rem'}>Verify OTP</Text>
             <Flex direction="column" justify="centre"
                 align="center" gap={{ sm: 'lg' }}>
                 <Text size="1.4rem" c={'white'}  >An OTP has been sent to your email
                 </Text>
-                <Text size="1rem" style={{ color: '#9441D0',marginBottom: '1rem' }}>Resend OTP in {resendTime} seconds</Text>
-                {/* <a href="/login" style={{ color: '#9441D0' }}>Log In</a> */}
+                <Button
+                disabled={resendTime > 0} // Disable button if resend timer is active
+                onClick={handleResendOtp}
+                style={{ marginTop: '1rem', width: 'fit-content', background: 'none', borderRadius: '1rem', fontSize: '1rem' }}
+            >
+                {resendTime === 0 ? "Resend OTP" : `Resend OTP in ${resendTime} seconds`}
+            </Button>
+
             </Flex>
             <Box style={{
                 display: 'flex',
@@ -138,41 +159,18 @@ export function Otp({ initialValues }: any) {
             }}>
                 {/* <form style={{display:"flex", flexDirection:"column"}} onSubmit={form.onSubmit((values) => console.log(values))}> */}
                 <div style={{ marginTop: "2rem",display: "flex", justifyContent: "space-evenly", alignItems: "center" }}>
-                    {/* <TextInput
-                    required
-                    label="OTP"
-                    placeholder="Enter OTP"
-                    value={form.values.name}
-                    onChange={(event) => form.setFieldValue('name', event.currentTarget.value)}
-                    error={form.errors.name && 'Required'}
-                    radius="md"
-                    size="lg"
-                    style={{ width: '45%', color: 'white' }}
-                    styles={{
-                        input: {
-                            background: 'transparent',
-                            color: 'white',
-                            borderColor: 'purple',
-                        },
-                    }}
-                /> */}
+                    
                     <PinInput size="xl" placeholder='_' type="number"
                         value={otpValue}
                         length={6}
                         onChange={(value) => handleOtpChange(value)} />
                 </div>
 
-
-
-
-                {/* <Text style={{position:"absolute", paddingTop:"10rem"}} size="1rem" c={'white'} >Create new account</Text> */}
-                <Button style={{ marginTop: '1rem', width: '50%', height: '3rem', backgroundColor: '#9441D0', borderRadius: '1rem', fontSize: '1rem' }} onClick={() => { handleOtp() }}  >Verify</Button>
+                <Button style={{ marginTop: '2rem', width: '50%', height: '3rem', backgroundColor: '#9441D0', borderRadius: '1rem', fontSize: '1rem',marginBottom: '1.5rem' }} onClick={() => { handleOtp() }}  >Verify</Button>
                 <div style={{ display: "flex", flexDirection: "column", justifyContent: "center", paddingBottom: "1.5%" }}>
-                    {/* <Divider label="Or continue with Google" labelPosition="center" my="lg" /> */}
-                    {/* <h6 style={{height:"0px"}}>Or continue with Google</h6>
-               <GoogleButton radius="xl">Google</GoogleButton> */}
+
                 </div>
-                {/* </form> */}
+
 
             </Box>
         </Flex>

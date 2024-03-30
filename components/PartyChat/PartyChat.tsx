6c891d806950e11e605f52d31260c24093543e27
Rohan Barsagade <rbarsagade101@gmail.com>
2024-03-30 21:43:50 +0530

    update party chat ui

--- components/PartyChat/PartyChat.tsx
+++ components/PartyChat/PartyChat.tsx
@@ -1,30 +1,29 @@
-'use client';
-import { useState } from 'react';
-import ChatTab from './ChatTab';
-import SettingsTab from './SettingsTab';
-import style from './PartyChat.module.css';
-import usePlayerStore from '@/Stores/PlayerStore';
+"use client";
+import { useState } from "react";
+import ChatTab from "./ChatTab";
+import SettingsTab from "./SettingsTab";
+import style from "./PartyChat.module.css";
 
 export default function PartyChat({ ws }: { ws: WebSocket }) {
-  const [activeTab, setActiveTab] = useState(0);
+    const [activeTab, setActiveTab] = useState(0);
 
-  return (
-    <div className={style.container}>
-      <div className={style.tabcontainer}>
-        <button
-          onClick={() => setActiveTab(0)}
-          className={activeTab == 0 ? style.active : style.tab}
-        >
-          Chat
-        </button>
-        <button
-          onClick={() => setActiveTab(1)}
-          className={activeTab == 1 ? style.active : style.tab}
-        >
-          Settings
-        </button>
-      </div>
-      {activeTab == 0 ? <ChatTab ws={ws} /> : <SettingsTab />}
-    </div>
-  );
+    return (
+        <div className={style.container}>
+            <div className={style.tabcontainer}>
+                <button
+                    onClick={() => setActiveTab(0)}
+                    className={activeTab == 0 ? style.active : style.tab}
+                >
+                    Chat
+                </button>
+                <button
+                    onClick={() => setActiveTab(1)}
+                    className={activeTab == 1 ? style.active : style.tab}
+                >
+                    Settings
+                </button>
+            </div>
+            {activeTab == 0 ? <ChatTab ws={ws} /> : <SettingsTab />}
+        </div>
+    );
 }
